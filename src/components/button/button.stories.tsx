ba7f553e1e9326d957d2387f0f4a4d2ef7f47c23
gioboa <giorgiob.boa@gmail.com>
2023-06-07 15:32:32 +0200

    add Button story

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,17 +1,25 @@
 import type { Meta, StoryObj } from 'storybook-framework-qwik';
-import { Button, type ButtonProps } from './button';
+import { Button, type Props } from './button';
 
-const meta: Meta<ButtonProps> = {
-  component: Button,
+const meta: Meta<Props> = {
+	component: Button,
 };
 
-type Story = StoryObj<ButtonProps>;
+type Story = StoryObj<Props>;
 
 export default meta;
 
-export const Primary: Story = {
-  args: {
-    size: 'medium',
-  },
-  render: (props) => <Button {...props}>Some button</Button>,
+export const Small: Story = {
+	args: { size: 'sm' },
+	render: (props) => <Button {...props}>Some button</Button>,
+};
+
+export const Medium: Story = {
+	args: { size: 'md' },
+	render: (props) => <Button {...props}>Some button</Button>,
+};
+
+export const Large: Story = {
+	args: { size: 'lg' },
+	render: (props) => <Button {...props}>Some button</Button>,
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,27 +1,38 @@
-import { component$, useStylesScoped$, Slot } from '@builder.io/qwik';
+import type { PropFunction } from '@builder.io/qwik';
+import { Slot, component$, useStyles$ } from '@builder.io/qwik';
+import styles from './button.scss?inline';
 
-export interface ButtonProps {
-  size?: 'small' | 'medium' | 'large';
-}
-export const Button = component$<ButtonProps>(({ size = 'medium' }) => {
-  useStylesScoped$(`
-    .size-small {
-      font-size: 10px;
-    }
-    .size-medium {
-      font-size: 14px;
-    }
-    .size-large {
-      font-size: 18px;
-    }
-  `);
-  return (
-    <button
-      class={{
-        [`size-${size}`]: true,
-      }}
-    >
-      <Slot></Slot>
-    </button>
-  );
-});
+export const config = {
+	sizes: {
+		sm: 'small',
+		md: 'medium',
+		lg: 'large',
+	},
+};
+
+export type ButtonSizes = 'sm' | 'md' | 'lg';
+
+export type Props = {
+	type: 'button' | 'reset' | 'submit';
+	size: ButtonSizes;
+	onClick$: PropFunction<() => void>;
+	class: string;
+	disabled: boolean;
+};
+
+export const Button = component$<Props>(
+	({ type = 'button', onClick$, disabled, size = 'md' }) => {
+		useStyles$(styles);
+
+		return (
+			<button
+				type={type}
+				onClick$={onClick$}
+				disabled={disabled}
+				class={`button ${config.sizes[size]}`}
+			>
+				<Slot />
+			</button>
+		);
+	}
+);
